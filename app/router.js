240b738dd79c5e7894c3c2b55791c6d899258514
rafaelangical <rsnrafael@outlook.com>
2018-05-16 23:31:19 -0300

    New routes

--- app/router.js
+++ app/router.js
@@ -1,18 +1,14 @@
 import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
-
-const Router = EmberRouter.extend({
-  location: config.locationType,
-  rootURL: config.rootURL
-});
-
+const Router = EmberRouter.extend({ location: config.locationType, rootURL: config.rootURL });
 Router.map(function() {
-  this.route('about');
-  this.route('contact');
-
-  this.route('admin', function() {
-    this.route('invitations');
-  });
+	this.route('about');
+	this.route('contact');
+	this.route('admin', function() {
+		this.route('invitations');
+	});
+	this.route('libraries', function() {
+		this.route('new');
+	});
 });
-
 export default Router;
