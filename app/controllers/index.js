8880de960526bcacac1203c1c64a5340f202bbae
rafaelangical <rsnrafael@outlook.com>
2018-05-16 21:36:37 -0300

    Update to new controller string

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,18 +1,21 @@
 import Controller from '@ember/controller';
 import { match, not } from '@ember/object/computed';
 export default Controller.extend({
+	headerMessage: 'Coming Soon',
 	responseMessage: '',
 	emailAddress: '',
 	isValid: match('emailAddress', /^.+@.+ \. .+$/),
 	isDisabled: not('isValid'),
 	actions: {
 		saveInvitation() {
-			alert(` Saving of the following email address is in progress : $ { this . get ( 'emailAddress' )} `);
+			const email = this.get('emailAddress');
+			const newInvitation = this.store.createRecord('invitation', { email: email });
+			newInvitation.save();
 			this.set(
 				'responseMessage',
-				` Thank you ! We 've just saved your email address: ${this.get(' emailAddress ')}`
+				` Thank you ! We have just saved your email address : $ { this . get ( 'emailAddress' )} `
 			);
-			this.set(' emailAddress ', ' ');
+			this.set('emailAddress', '');
 		}
 	}
 });
