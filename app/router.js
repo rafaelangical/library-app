a9f0835d515e3b293035c45c5983e22e925f7486
rafaelangical <rsnrafael@outlook.com>
2018-05-22 00:33:08 -0300

    Create new page of admin

--- app/router.js
+++ app/router.js
@@ -2,15 +2,16 @@ import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 const Router = EmberRouter.extend({ location: config.locationType, rootURL: config.rootURL });
 Router.map(function() {
-	this.route('about');
-	this.route('contact');
-	this.route('admin', function() {
-		this.route('invitations');
-		this.route('contacts');
-	});
-	this.route('libraries', function() {
-		this.route('new');
-		this.route('edit', { path: '/:library_id/edit' });
-	});
+    this.route('about');
+    this.route('contact');
+    this.route('admin', function() {
+      this.route('invitations');
+      this.route('contacts');
+      this.route('seeder');
+    });
+    this.route('libraries', function() {
+        this.route('new');
+        this.route('edit', { path: '/:library_id/edit' });
+    });
 });
 export default Router;
