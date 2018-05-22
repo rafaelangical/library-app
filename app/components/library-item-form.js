5aba264a1f0a31b1f0ff9346deb4ef92cde3ac54
rafaelangical <rsnrafael@outlook.com>
2018-05-22 00:21:18 -0300

    Componentized all

--- app/components/library-item-form.js
+++ app/components/library-item-form.js
@@ -1,4 +1,9 @@
 import Component from '@ember/component';
-
 export default Component.extend({
+	buttonLabel: 'Save',
+	actions: {
+		buttonClicked(param) {
+			this.sendAction('action', param);
+		}
+	}
 });
--- app/models/library.js
+++ app/models/library.js
@@ -1,7 +1,8 @@
 import DS from 'ember-data';
-
+import { notEmpty } from '@ember/object/computed';
 export default DS.Model.extend({
-  name: DS.attr('string'),
-  address: DS.attr('string'),
-  phone: DS.attr('string')
+	name: DS.attr('string'),
+	address: DS.attr('string'),
+	phone: DS.attr('string'),
+	isValid: notEmpty('name')
 });
