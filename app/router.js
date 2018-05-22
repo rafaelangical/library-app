9c40224534823ba19be245589f5214928da7a38b
rafaelangical <rsnrafael@outlook.com>
2018-05-21 23:55:28 -0300

    Changes

--- app/router.js
+++ app/router.js
@@ -6,9 +6,11 @@ Router.map(function() {
 	this.route('contact');
 	this.route('admin', function() {
 		this.route('invitations');
+		this.route('contacts');
 	});
 	this.route('libraries', function() {
 		this.route('new');
+		this.route('edit', { path: '/:library_id/edit' });
 	});
 });
 export default Router;
