ac22023e3aa78b237baca02856abfeaa61413c18
rafaelangical <rsnrafael@outlook.com>
2018-06-19 17:31:56 -0300

    Identacion clean up

--- app/routes/login.js
+++ app/routes/login.js
@@ -2,17 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   actions: {
-       login: function() {
-         const controller = this.get('controller');
-         const email = controller.get('userEmail');
-         const password = controller.get('userPassword');
-           this.get('session').open('firebase', {
-                provider: 'password',
-                email: email,
-                password: password
-           }).then(function() {
-               this.transitionTo('/');
-           }.bind(this));
-       }
+    login: function() {
+      const controller = this.get('controller');
+      const email = controller.get('userEmail');
+      const password = controller.get('userPassword');
+        this.get('session').open('firebase', {
+             provider: 'password',
+             email: email,
+             password: password
+        }).then(function() {
+            this.transitionTo('/');
+        }.bind(this));
+    }
    }
 });
