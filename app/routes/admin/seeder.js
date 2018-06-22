3c5e9aa24656f6a5b1642e3b76412cd90b0e8bbf
rafaelangical <rsnrafael@outlook.com>
2018-06-22 20:15:49 -0300

    Update seeder route

--- app/routes/admin/seeder.js
+++ app/routes/admin/seeder.js
@@ -1,5 +1,6 @@
 import { hash } from 'rsvp';
 import Route from '@ember/routing/route';
+import Configuration from  './configuration';
 
 export default Route.extend({
 
@@ -35,7 +36,17 @@ export default Route.extend({
   //activate() {
   //  debugger;
   //}
-
+  beforeModel(transition) {
+    if (!this.get('session.isAuthenticated')) {
+      Ember.assert('The route configured as Configuration.authenticationRoute cannot implement the AuthenticatedRouteMixin mixin as that leads to an infinite transitioning loop!', this.get('routeName') !== Configuration.authenticationRoute);
+      transition.abort();
+      this.set('session.attemptedTransition', transition);
+      this.transitionTo('login');
+    
+      } else {
+        return this._super(...arguments);
+      }
+  },
   model() {
     return hash({
       libraries: this.store.findAll('library'),
