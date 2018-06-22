15187f4b67a6f53ed7178199379f7f78eaf4f828
rafaelangical <rsnrafael@outlook.com>
2018-06-22 20:15:24 -0300

    Update contact route

--- app/routes/admin/contacts.js
+++ app/routes/admin/contacts.js
@@ -1,7 +1,18 @@
 import Route from '@ember/routing/route';
+import Configuration from  './configuration';
 
 export default Route.extend({
+  beforeModel(transition) {
+    if (!this.get('session.isAuthenticated')) {
+      Ember.assert('The route configured as Configuration.authenticationRoute cannot implement the AuthenticatedRouteMixin mixin as that leads to an infinite transitioning loop!', this.get('routeName') !== Configuration.authenticationRoute);
 
+      transition.abort();
+      this.set('session.attemptedTransition', transition);
+      this.transitionTo('login');
+    } else {
+      return this._super(...arguments);
+    }
+  },
   model() {
     return this.store.findAll('contact');
   }
